66c8fa9bc3bff59c63a411862c19efecf32f2a04
Elsa Menzel <menzel.elsa@yandex.ru>
2023-04-30 22:21:52 +0300

    Добавление ComboBox в main window, копирование кода из ManagerWindow & ConsultantWindow в MainWindow. удаление окон менеджера и консультанта.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,23 +23,83 @@ namespace Homework11_WPF.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Worker> peopleList { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void buttonConsultant_Click(object sender, RoutedEventArgs e)
+        private void comboBoxChoice_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ConsultantWindow consultantWindow = new ConsultantWindow();
-            consultantWindow.Show();
-            this.Close();
+            ComboBoxItem _comboBoxItem = (ComboBoxItem)comboBoxChoice.SelectedItem;
+            string _equalValue = _comboBoxItem.Content.ToString();
+
+            XDocument xDocument = XDocument.Load("people.xml");
+
+            if (_equalValue == "Консультант")
+            {
+                ReadXmlFile(xDocument,_equalValue);
+            }
+            if (_equalValue == "Менеджер")
+            {
+                ReadXmlFile(xDocument,_equalValue);
+            }
         }
 
-        private void buttonManager_Click(object sender, RoutedEventArgs e)
+        private void ReadXmlFile(XDocument xDocument, string equalValue)
         {
-            ManagerWindow managerWindow = new ManagerWindow();
-            managerWindow.Show();
-            this.Close();
+            XElement? people = xDocument.Element("People");
+
+            peopleList = new ObservableCollection<Worker>();
+            int index = (from xelement in xDocument.Root.Descendants("Person") select xelement).Count(); //подсчет количества дочерних узлов в "Person"
+
+            if (!(people is null))
+            {
+                bool resultConsultant = equalValue == "Консультант";
+                bool resultManager = equalValue == "Менеджер";
+                Consultant[] consultant = new Consultant[index];
+                Manager[] manager = new Manager[index];
+
+                foreach (XElement person in people.Elements("Person"))
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        XElement? SurnamePersone = person.Element("Surname");
+                        XElement? FirstNamePersone = person.Element("FirstName");
+                        XElement? LastNamePersone = person.Element("LastName");
+                        XElement? PassportDataPersone = person.Element("PassportData");
+                        XElement? PhoneNumberPersone = person.Element("MobilePhone");
+
+                        if (resultConsultant == true)
+                        {
+                            consultant[i] = new Consultant();
+                            consultant[i].Surname = SurnamePersone.Value;
+                            consultant[i].FirstName = FirstNamePersone.Value;
+                            consultant[i].LastName = LastNamePersone.Value;
+                            if (PassportDataPersone.Value != "")
+                                consultant[i].PasportData = PassportDataPersone.Value;
+                            if (PhoneNumberPersone.Value != "")
+                                consultant[i].PhoneNumber = double.Parse(PhoneNumberPersone.Value);
+                            peopleList.Add(consultant[i]);
+                        }
+
+                        if (resultManager == true)
+                        {
+                            manager[i] = new Manager();
+                            manager[i].Surname = SurnamePersone.Value;
+                            manager[i].FirstName = FirstNamePersone.Value;
+                            manager[i].LastName = LastNamePersone.Value;
+                            if (PassportDataPersone.Value != "")
+                                manager[i].PasportData = PassportDataPersone.Value;
+                            if (PhoneNumberPersone.Value != "")
+                                manager[i].PhoneNumber = double.Parse(PhoneNumberPersone.Value);
+                            peopleList.Add(manager[i]);
+                        }
+                    }
+                }
+            }
+            dataGridListPerson.ItemsSource = peopleList;
         }
     }
 }
